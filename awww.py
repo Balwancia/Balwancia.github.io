d3690b86fdb6190cab43bf18fa099c61438b85ed
Anna Piórkowska <ap448450@students.mimuw.edu.pl>
2024-03-05 19:37:07 +0100

    kolejny commit

--- awww.py
+++ awww.py
@@ -4,7 +4,7 @@ href = 'https://www.tiobe.com/tiobe-index/'
 r = requests.get(href)
 #print(r.text)
 soup = BeautifulSoup(r.text, 'html.parser')
-soup1 = soup.find_all('img')
+soup1 = soup.find_all('table')
 print(soup1)
 
 # print(soup.prettify())
