7fbb3da7a8ef05fb43cbfc3d652377eb79172aef
Anna Piórkowska <ap448450@students.mimuw.edu.pl>
2024-03-05 21:00:17 +0100

    kolejny commit

--- awww.py
+++ awww.py
@@ -2,9 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 href = 'https://www.tiobe.com/tiobe-index/'
 r = requests.get(href)
-#print(r.text)
-soup = BeautifulSoup(r.text, 'html.parser')
-soup1 = soup.find_all('table')
-print(soup1)
+print('--- # AWWW ---')
+print('--- Anna Piórkowska ---')
+print('--- [20 najlpopularnijeszych jezyków programowania](balwancia.github.io/jezyki-programowania) ---')
+# prsint(r.text)
+
+# soup = BeautifulSoup(r.text, 'html.parser')
+# soup1 = soup.find_all('table')
+# print(soup1)
 
 # print(soup.prettify())
