9dcbbb140ee9184dc8f9e65bcb087cf072de7ab4
Anna Piórkowska <ap448450@students.mimuw.edu.pl>
2024-03-05 19:12:36 +0100

    kolejny commit

--- awww.py
+++ awww.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 href = 'https://www.tiobe.com/tiobe-index/'
 r = requests.get(href)
+#print(r.text)
 soup = BeautifulSoup(r.text, 'html.parser')
-soup1 = soup.find_all('a')
+soup1 = soup.find_all('img')
 print(soup1)
 
 # print(soup.prettify())
