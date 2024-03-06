909fee14fead62793cc9716db24ac69e5a2b0757
Anna Piórkowska <ap448450@students.mimuw.edu.pl>
2024-03-06 22:23:48 +0100

    commit

--- jezyki.py
+++ jezyki.py
@@ -10,7 +10,7 @@ tab = soup.find('table').find('tbody').find_all('tr')
 
 with open(f'index.md', 'w') as index:
     index.write('# Witaj na mojej stronie, dla takiej osoby jak Ty, to warto się roztopić!\n' + 
-                '[20 najpopularniejszych jezyków programowania](../jezyki_programowania)\n' +
+                '[20 najpopularniejszych jezyków programowania](../jezyki_programowania)' +
                 '![](https://images.emojiterra.com/google/android-12l/512px/2603.png)') 
 
 with open(f'jezyki_programowania.md', 'w') as lista:
