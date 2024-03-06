9b58fd9fdd180e9c88d4828bee969ccf5e2d8387
Anna Piórkowska <ap448450@students.mimuw.edu.pl>
2024-03-06 22:26:56 +0100

    commit

--- cos.py
+++
@@ -1,3 +0,0 @@
-<tr><td>16</td><td>15</td><td><img alt="change" src="/wp-content/themes/tiobe/tpci/images/down.png"/></td>
-<td class="td-top20"><img alt="Swift page" src="/wp-content/themes/tiobe/tiobe-index/images/Swift.png" style="vertical-align:middle"/></td>
-<td>Swift</td><td>1.16%</td><td>+0.23%</td></tr>
